ccc245e36d402ea1b386cbb23ff5f9e552828799
jinhyeong830 <shg02387@naver.com>
2025-04-08 01:47:48 +0900

    complete docker local test

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,28 @@
-# 이미지 정의
-FROM node:18-alpine
+#### [step1]
+# node 18버전 환경을 builder라는 별칭사용함(alias)
+FROM node:18-alpine AS builder
 
-# 작업 공간
+# 작업 공간 /app
 WORKDIR /app
 
-# package.json 파일을 컨테이너로 복사
-COPY package.json .
-
-# npm install 명령 실행 (의존성 설치)
-RUN npm install
-
-# 프로젝트의 모든 파일을 컨테이너로 복사
+# 현재 폴더에 있는 모든 하위 파일들을 /app 컨테이너에 복사
 COPY . .
 
-# 3000 포트 외부에 노출
-EXPOSE 3000
+# 컨테이너에 package.json이 있으므로 해당 파일을 보고 의존성 설치
+RUN npm install
 
-# npm start 명령을 실행하여 프로젝트 시작
-CMD ["npm", "start"]
+# 의존성 설치 후 빌드를 통해 정적파일 생성
+RUN npm run build
+
+#### [step2]
+# nginx 이미지로부터 시작되는 정적파일 배포
+FROM nginx:alpine
+
+# builder는 1에서 지어줬던 별칭, build 파일을 ngin/html 로 복사함함
+COPY --from=builder /app/build /usr/share/nginx/html
+
+# 80번 포트 외부에 노출
+EXPOSE 80
+
+# nginx 서버 시작
+CMD ["nginx", "-g", "daemon off;"]
--- src/components/organism/Form.tsx
+++ src/components/organism/Form.tsx
@@ -1,17 +1,17 @@
-import { useForm } from 'react-hook-form';
-import InputText from '../atoms/InputText';
-import LabelWithInput from '../molecules/LabelWithInput';
-import 'axios';
-import { FormType } from '../../constants/enum';
-import { classJoin } from '../../utils/utils';
-import Button from '../atoms/Button';
-import Heading2 from '../atoms/Heading2';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import { useRecoilState, useSetRecoilState } from 'recoil';
-import { authState } from '../../recoil/authState';
-import { AuthInterface } from '../../interface/interface';
-import Cookies from 'js-cookie';
+import { useForm } from "react-hook-form";
+import InputText from "../atoms/InputText";
+import LabelWithInput from "../molecules/LabelWithInput";
+import "axios";
+import { FormType } from "../../constants/enum";
+import { classJoin } from "../../utils/utils";
+import Button from "../atoms/Button";
+import Heading2 from "../atoms/Heading2";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useRecoilState, useSetRecoilState } from "recoil";
+import { authState } from "../../recoil/authState";
+import { AuthInterface } from "../../interface/interface";
+import Cookies from "js-cookie";
 
 interface Props {
   type: FormType;
@@ -38,13 +38,18 @@ export default function Form({ type }: Props) {
     const { data }: { data: AuthInterface } = await axios.post(
       `${process.env.REACT_APP_API_SERVER}/auth/signin`,
       submitData,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
     );
     console.log(data);
 
     // 토큰 쿠키 저장
     if (data.token && data.id) {
-      Cookies.set('token', data.token);
-      Cookies.set('userid', data.id.toString());
+      Cookies.set("token", data.token);
+      Cookies.set("userid", data.id.toString());
     }
 
     // 로그인 상태 저장
@@ -55,26 +60,31 @@ export default function Form({ type }: Props) {
     });
 
     // 로그인 성공시 홈으로
-    navigate('/');
+    navigate("/");
   };
 
   const doRegister = async (submitData: SubmitInterface) => {
     const response = await axios.post(
       `${process.env.REACT_APP_API_SERVER}/auth/signup`,
       submitData,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
     );
     console.log(response.data);
-    navigate('/signin');
+    navigate("/signin");
   };
 
   const submitSuccess = (data: SubmitInterface) => {
     console.log(data);
     try {
-      FormType.SIGNIN ? doLogin(data) : doRegister(data);
+      type === FormType.SIGNIN ? doLogin(data) : doRegister(data);
     } catch (err: any) {
       console.error(err.status);
       if (err.status === 400) {
-        alert('존재하는 email 입니다.');
+        alert("존재하는 email 입니다.");
       }
       reset(); // 폼
     }
@@ -87,8 +97,8 @@ export default function Form({ type }: Props) {
   return (
     <form
       className={classJoin(
-        type === FormType.SIGNIN ? 'bg-indigo-100' : 'bg-amber-100',
-        'shadow-md  shadow-[#ddddddc6] border border-[#dddddd95] rounded-md max-w-[500px] mx-auto px-20 py-14',
+        type === FormType.SIGNIN ? "bg-indigo-100" : "bg-amber-100",
+        "shadow-md  shadow-[#ddddddc6] border border-[#dddddd95] rounded-md max-w-[500px] mx-auto px-20 py-14"
       )}
       onSubmit={handleSubmit(submitSuccess, submitFail)}
     >
@@ -100,9 +110,9 @@ export default function Form({ type }: Props) {
           <InputText
             type="email"
             id="email"
-            register={register('email', {
+            register={register("email", {
               onChange: () => {
-                console.log('onchange ... ing');
+                console.log("onchange ... ing");
               },
               required: true,
             })}
@@ -113,9 +123,9 @@ export default function Form({ type }: Props) {
             <InputText
               type="text"
               id="username"
-              register={register('username', {
+              register={register("username", {
                 onChange: () => {
-                  console.log('onchange ... ing');
+                  console.log("onchange ... ing");
                 },
                 required: true,
               })}
@@ -126,9 +136,9 @@ export default function Form({ type }: Props) {
           <InputText
             type="password"
             id="password"
-            register={register('password', {
+            register={register("password", {
               onChange: () => {
-                console.log('onchange ... ing');
+                console.log("onchange ... ing");
               },
               required: true,
             })}
@@ -136,14 +146,14 @@ export default function Form({ type }: Props) {
         </LabelWithInput>
       </div>
 
-      {errors.email && 'email 에 뭔가 에러가 있어요..'}
-      {errors.password && 'password 에 뭔가 에러가 있어요..'}
-      {errors.username && 'username 에 뭔가 에러가 있어요..'}
+      {errors.email && "email 에 뭔가 에러가 있어요.."}
+      {errors.password && "password 에 뭔가 에러가 있어요.."}
+      {errors.username && "username 에 뭔가 에러가 있어요.."}
       <div className="text-center mt-10">
         <Button
           type="submit"
           className={classJoin(
-            type === FormType.SIGNIN ? 'bg-indigo-600' : 'bg-amber-500',
+            type === FormType.SIGNIN ? "bg-indigo-600" : "bg-amber-500"
           )}
         >
           {type === FormType.SIGNIN ? FormType.SIGNIN : FormType.SIGNUP}
